0c7a9c3b1eb91d00c2b24f1abf453d30159787e7
Joaquin Miranda <juacomiranda@MacBook-Air-de-Maria.local>
2023-06-14 12:14:34 -0400

    reloj

--- src/js/index.js
+++ src/js/index.js
@@ -11,12 +11,15 @@ function SimpleCounter(props) {
   return (
     <div className="container-fluid">
       <div className="big-counter">
-        <div className="seis"></div>
+        <div className="reloj">
+          <i className="far fa-clock"> </i>
+        </div>
+        <div className="seis">0</div>
         <div className="cinco">0</div>
         <div className="cuatro">0</div>
         <div className="tres">0</div>
         <div className="dos">0</div>
-        <div className="uno">1</div>
+        <div className="uno">0</div>
       </div>
     </div>
   );
